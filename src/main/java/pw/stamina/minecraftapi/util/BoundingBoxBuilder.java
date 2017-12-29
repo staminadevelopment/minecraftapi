f22444727b761a9ce81754cd03c80fc33f7c48a1
Abstraction <n3xuz@stamina.pw>
2017-12-29 07:51:22 +0100

    Added convenience methods in BoundingBoxBuilder
    Added methods in BoundingBoxBuilder that allows the user to specific both values for a dimension at the same time

--- src/main/java/pw/stamina/minecraftapi/util/BoundingBoxBuilder.java
+++ src/main/java/pw/stamina/minecraftapi/util/BoundingBoxBuilder.java
@@ -57,6 +57,24 @@ public final class BoundingBoxBuilder {
         return this;
     }
 
+    public BoundingBoxBuilder x(double x1, double x2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        return this;
+    }
+
+    public BoundingBoxBuilder y(double y1, double y2) {
+        this.y1 = y1;
+        this.y2 = y2;
+        return this;
+    }
+
+    public BoundingBoxBuilder z(double z1, double z2) {
+        this.z1 = z1;
+        this.z2 = z2;
+        return this;
+    }
+
     public BoundingBox build() {
         return BoundingBox.fromBounds(x1, y1, z1, x2, y2, z2);
     }
