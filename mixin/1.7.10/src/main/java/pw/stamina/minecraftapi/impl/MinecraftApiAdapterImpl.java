eec6dc8222bd3696d8b3aaee9d9b8d5926b6375c
Abstraction <n3xuz@stamina.pw>
2017-12-29 06:31:08 +0100

    Removed the old version specific implementations Added adapter implementations for each version

--- mixin/1.7.10/src/main/java/pw/stamina/minecraftapi/impl/MinecraftApiAdapterImpl.java
+++ mixin/1.7.10/src/main/java/pw/stamina/minecraftapi/impl/MinecraftApiAdapterImpl.java
@@ -24,18 +24,20 @@
 
 package pw.stamina.minecraftapi.impl;
 
-import com.google.auto.service.AutoService;
 import net.minecraft.util.AxisAlignedBB;
+import pw.stamina.minecraftapi.MinecraftApiAdapter;
+import pw.stamina.minecraftapi.client.Minecraft;
 import pw.stamina.minecraftapi.util.BoundingBox;
-import pw.stamina.minecraftapi.util.BoundingBoxCreationService;
-import pw.stamina.minecraftapi.service.MinecraftApiService;
 
-@AutoService(MinecraftApiService.class)
-public final class BoundingBoxCreationServiceImpl implements BoundingBoxCreationService {
+public final class MinecraftApiAdapterImpl implements MinecraftApiAdapter {
 
     @Override
-    public BoundingBox fromBounds(double x1, double y1, double z1,
-                                  double x2, double y2, double z2) {
-        return (BoundingBox) AxisAlignedBB.getBoundingBox(x1, y1, z1, x2, y2, z2);
+    public Minecraft getMinecraft() {
+        return (Minecraft) net.minecraft.client.Minecraft.getMinecraft();
+    }
+
+    @Override
+    public BoundingBox.Factory getBoundingBoxFactory() {
+        return (x1, y1, z1, x2, y2, z2) -> (BoundingBox) AxisAlignedBB.getBoundingBox(x1, y1, z1, x2, y2, z2);
     }
 }
