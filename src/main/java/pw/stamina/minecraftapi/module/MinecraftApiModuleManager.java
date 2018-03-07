9840837bc474b6b3bb1caa4b75b172547c552890
Lil' Pepo <n3xuz@stamina.pw>
2018-03-07 18:31:00 +0100

    Fix get event consumer from modules
    Add CompoundEventConsumer

--- src/main/java/pw/stamina/minecraftapi/module/MinecraftApiModuleManager.java
+++ src/main/java/pw/stamina/minecraftapi/module/MinecraftApiModuleManager.java
@@ -2,7 +2,11 @@ package pw.stamina.minecraftapi.module;
 
 import pw.stamina.minecraftapi.MinecraftApiAdapter;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.ServiceLoader;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public final class MinecraftApiModuleManager {
     private final ServiceLoader<MinecraftApiModule> modules;
@@ -34,6 +38,23 @@ public final class MinecraftApiModuleManager {
     }
 
     private static EventConsumer getEventConsumerFromModules(ServiceLoader<MinecraftApiModule> modules) {
-        return EventConsumer.empty(); //TODO: Get actual consumer
+        List<EventConsumer> eventConsumers = getAllEventConsumerFromModules(modules);
+
+        switch (eventConsumers.size()) {
+            case 0:
+                return EventConsumer.empty();
+            case 1:
+                return eventConsumers.get(0);
+            default:
+                return EventConsumer.compound(eventConsumers);
+        }
+    }
+
+    private static List<EventConsumer> getAllEventConsumerFromModules(ServiceLoader<MinecraftApiModule> modules) {
+        return StreamSupport.stream(modules.spliterator(), false)
+                .map(MinecraftApiModule::getEventConsumer)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
     }
 }
