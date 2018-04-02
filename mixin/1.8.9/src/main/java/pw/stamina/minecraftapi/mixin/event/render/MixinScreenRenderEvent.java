4592f44cb12788d8151ce0dddfaab93b1a3288d9
LilPepo <15078089+LilPepo@users.noreply.github.com>
2018-04-02 16:20:20 +0200

    Update copyright year

--- mixin/1.8.9/src/main/java/pw/stamina/minecraftapi/mixin/event/render/MixinScreenRenderEvent.java
+++ mixin/1.8.9/src/main/java/pw/stamina/minecraftapi/mixin/event/render/MixinScreenRenderEvent.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2017 Stamina Development
+ * Copyright (c) 2018 Stamina Development
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
