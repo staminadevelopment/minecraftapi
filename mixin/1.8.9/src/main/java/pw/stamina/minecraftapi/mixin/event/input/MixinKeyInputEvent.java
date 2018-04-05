fee821f5925a1531fcaf32bcdcc10805188396e3
Lil' Pepo <15078089+LilPepo@users.noreply.github.com>
2018-04-05 13:06:48 +0200

    Input events (#14)
    * Add key input event and input type
    
    * Add mouse input event
    
    * Add KeyInputEvent mixin
    
    * Add MouseInputEvent mixin
    
    * Clean up mixin code

---
+++ mixin/1.8.9/src/main/java/pw/stamina/minecraftapi/mixin/event/input/MixinKeyInputEvent.java
@@ -0,0 +1,39 @@
+package pw.stamina.minecraftapi.mixin.event.input;
+
+import net.minecraft.client.Minecraft;
+import org.lwjgl.input.Keyboard;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import pw.stamina.minecraftapi.MinecraftApi;
+import pw.stamina.minecraftapi.event.input.KeyInputEvent;
+import pw.stamina.minecraftapi.event.input.KeyInputType;
+
+@Mixin(Minecraft.class)
+public class MixinKeyInputEvent {
+
+    @Inject(method = "runTick", remap = false,
+            at = @At(value = "INVOKE",
+                    target = "Lorg/lwjgl/input/Keyboard;getEventKey()I",
+                    ordinal = 0))
+    private void emitKeyInputEvent(CallbackInfo cbi) {
+        MinecraftApi.emitEvent(new KeyInputEvent(getKeyCode(), getKeyInputType()));
+    }
+
+    private static int getKeyCode() {
+        return Keyboard.getEventKey() == 0 ? Keyboard.getEventCharacter() + 256 : Keyboard.getEventKey();
+    }
+
+    private static KeyInputType getKeyInputType() {
+        boolean pressed = Keyboard.getEventKeyState();
+
+        if (Keyboard.isRepeatEvent()) {
+            return KeyInputType.REPEAT;
+        } else if (pressed) {
+            return KeyInputType.PRESS;
+        } else {
+            return KeyInputType.RELEASE;
+        }
+    }
+}
