7d96a6395d599c84a9f5d56f36234cfa20bba6e8
LilPepo <15078089+LilPepo@users.noreply.github.com>
2018-04-05 13:14:19 +0200

    Improve lightmap related APIs

--- mixin/1.8.9/src/main/java/pw/stamina/minecraftapi/mixin/render/MixinEntityRenderer.java
+++ mixin/1.8.9/src/main/java/pw/stamina/minecraftapi/mixin/render/MixinEntityRenderer.java
@@ -31,10 +31,5 @@ import pw.stamina.minecraftapi.render.EntityRenderer;
 @Mixin(net.minecraft.client.renderer.EntityRenderer.class)
 public abstract class MixinEntityRenderer implements EntityRenderer {
 
-    @Override
-    public void setLightmapUpdateNeeded() {
-        this.lightmapUpdateNeeded = true;
-    }
 
-    @Shadow private boolean lightmapUpdateNeeded;
 }
